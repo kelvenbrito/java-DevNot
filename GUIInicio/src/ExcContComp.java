f4310f4adf42a0b0d143eb0f7dfc4c53ab2743e1
kelvenbrito <kelvenbrito14@gmail.com>
2023-08-22 22:02:41 -0300

    Correcao-22/08

--- GUIInicio/src/ExcContComp.java
+++ GUIInicio/src/ExcContComp.java
@@ -1,8 +1,9 @@
 
     
-    import javax.swing.JButton;
+    import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
@@ -27,12 +28,16 @@ public class ExcContComp  extends JFrame {
         but.addActionListener(e -> {
      
             clickCount++;
-             JButton but2 = new JButton(String.valueOf(clickCount));
+              JButton but2 = new JButton(String.valueOf(clickCount));
+             FlowLayout flow = new FlowLayout(); // Define o layout do container
+            this.setLayout(flow); // Seta layout do container
+           
               JPanel painel2 = new JPanel();
               painel.add(but2);
         
             this.getContentPane().add(painel2);
             this.pack();
+           
 
 
         });
